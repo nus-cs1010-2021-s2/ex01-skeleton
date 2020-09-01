02bcc0fc4be3237139d451004d65c5bfdc247b5c
Ooi Wei Tsang <weitsang@yahoo.com>
2020-09-01 05:21:33 +0000

    first commit

---
+++ gcd.c
@@ -0,0 +1,11 @@
+/**
+ * Read in two positive integers and find the greatest common divisors
+ * among them, recursively.
+ * @file: gcd.c
+ * @author: XXX (Group YYY)
+ */
+#include "cs1010.h"
+
+int main() 
+{
+}
